ac1ab1672dd2a76a5a3e3026d69a7d8d76f36fa4
screaming in agony <ameliar51@nycstudents.net>
2026-02-10 10:07:24 -0500

    day-1

---
+++ croissant.py
@@ -0,0 +1,23 @@
+
+""" store_items = {
+    item = {
+        "name": "evil tv",
+        "price": 500,
+        "department": "televisios",
+        "desc": "tv that eats your carpet"
+    },
+    item = {
+        "name": "sinister camera",
+        "price": 2,
+        "department": "cameras",
+        "desc": "this camera is broken but v pretty :33"
+    },
+    item = {
+        "name": "funky latop",
+        "price": 100000,
+        "department": "laptops",
+        "desc": "it explosions"
+    }
+} """
+
+store_items = {}
