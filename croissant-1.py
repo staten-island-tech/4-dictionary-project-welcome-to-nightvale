c3524975c9e6b6214ecf218316fab0267828be56
screaming in agony <ameliar51@nycstudents.net>
2026-02-11 10:15:58 -0500

    day-3?

--- croissant-1.py
+++ croissant-1.py
@@ -1,28 +1,25 @@
-
-""" store_items = {
-    "item1" = {
+""" 
+store_items = [
+    {
         "name": "evil tv",
         "price": 500,
         "department": "televisions",
         "desc": "tv that eats your carpet"
     },
-    "item2" = {
+ {
         "name": "sinister camera",
         "price": 2,
         "department": "cameras",
         "desc": "this camera is broken but v pretty :33"
     },
-    "item3" = {
+  {
         "name": "funky latop",
         "price": 100000,
         "department": "laptops",
         "desc": "it explosions"
     }
-} """
-#for some reson the above code does not work??
-#even though it is the exact same as below, which does work
-
-store_items = {
+] """
+""" store_items = {
     "item1" : {
         "name": "evil tv",
         "price": 500,
@@ -41,10 +38,40 @@ store_items = {
         "department": "laptops",
         "desc": "it explosions"
     },
-}
+} """
 
-""" print(store_items["item1"]["name"]) """
+
+store_items = [
+    {
+        "name": "evil tv",
+        "price": 500,
+        "department": "televisions",
+        "desc": "tv that eats your carpet"
+    },
+ {
+        "name": "sinister camera",
+        "price": 2,
+        "department": "cameras",
+        "desc": "this camera is broken but v pretty :33"
+    },
+  {
+        "name": "funky latop",
+        "price": 100000,
+        "department": "laptops",
+        "desc": "it explosions"
+    }
+]
+
+""" print(store_items[0]["name"]) """
 """ print(store_items) """
 
+print("welcome !! to evil store.")
 for index, item in enumerate(store_items):
     print(index, ":", item["name"])
+selection = int(input("select an item number to purchase!  "))
+def purchase(x):
+    for i in store_items:
+        if ('x = index'):
+            print(item["name"])
+
+purchase(selection)
