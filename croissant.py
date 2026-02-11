2129d8ddd2b3a03c26bb13c17c4ad7ffd891f65d
screaming in agony <ameliar51@nycstudents.net>
2026-02-10 19:49:10 -0500

    cries of despair

--- croissant.py
+++ croissant.py
@@ -1,23 +1,50 @@
 
 """ store_items = {
-    item = {
+    "item1" = {
         "name": "evil tv",
         "price": 500,
-        "department": "televisios",
+        "department": "televisions",
         "desc": "tv that eats your carpet"
     },
-    item = {
+    "item2" = {
         "name": "sinister camera",
         "price": 2,
         "department": "cameras",
         "desc": "this camera is broken but v pretty :33"
     },
-    item = {
+    "item3" = {
         "name": "funky latop",
         "price": 100000,
         "department": "laptops",
         "desc": "it explosions"
     }
 } """
+#for some reson the above code does not work??
+#even though it is the exact same as below, which does work
 
-store_items = {}
+store_items = {
+    "item1" : {
+        "name": "evil tv",
+        "price": 500,
+        "department": "televisions",
+        "desc": "tv that eats your carpet"
+    },
+    "item2" : {
+        "name": "sinister camera",
+        "price": 2,
+        "department": "cameras",
+        "desc": "this camera is broken but v pretty :33"
+    },
+    "item3" : {
+        "name": "funky latop",
+        "price": 100000,
+        "department": "laptops",
+        "desc": "it explosions"
+    },
+}
+
+""" print(store_items["item1"]["name"]) """
+""" print(store_items) """
+
+for index, item in enumerate(store_items):
+    print(index, ":", item["name"])
